9f10a23b96f37851ef48b9724da697b9218c538c
annabelous75 <57856008+annabelous75@users.noreply.github.com>
2022-12-08 23:01:02 +0200

    update 08.12

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+  let timerId=null;
+const refs={
+    start:document.querySelector("button[data-start]"),
+    stop:document.querySelector("button[data-stop]"),
+};
+refs.start.addEventListener("click", () => {
+    timerId = setInterval(() => {
+        const color=getRandomHexColor();
+        document.body.style.background=color;
+    }, 1000);
+    refs.start.disabled=true;
+    refs.stop.disabled=false;
+});
 
+
+refs.stop.addEventListener("click", () => {
+    clearInterval(timerId);
+    refs.stop.disabled=true;
+    refs.start.disabled=false;
+  });
